412159de20b86a431068d86c3657340f6edf28a3
Donald R. Williams <brycewayne03.29@gmail.com>
2021-06-08 05:55:40 -0700

    add git hub work flows

--- R/make_study_id.R
+++ R/make_study_id.R
@@ -1,3 +1,20 @@
+#' @title  Make \code{study_id} Variable
+#' 
+#' @description \link[blsmeta]{blsmeta} requires a numeric vector 
+#'               for the \code{study_id} variable create that variable from 
+#'               a character vector (for example).
+#' 
+#' @param x character vector
+#'
+#' @return numeric \code{study_id}
+#' 
+#' @export
+#'
+#' @examples
+#' # data
+#' library(psymetadata)
+#' 
+#' make_study_id( gnambs2020$author )
 make_study_id <- function(x){
   match(x, sort(unique(x)))
 }
